e8640434de11d55502df1f20a8c612bc864018cc
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-03-04 15:43:44 +0000

    add: new object Mailbox

---
+++ src/domain/Mailbox.java
@@ -0,0 +1,12 @@
+package src.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mailbox {
+	private List<Message> mailBox;
+	
+	public Mailbox() {
+		this.mailBox = new ArrayList<>();
+	}
+}
