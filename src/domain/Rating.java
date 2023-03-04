87dd2927d74afbedd89827573f3ffeece76ef8c1
Bruno Costa <82226653+BrunoHamilton44@users.noreply.github.com>
2023-03-04 17:47:28 +0000

    add: new object Rating

---
+++ src/domain/Rating.java
@@ -0,0 +1,23 @@
+package src.domain;
+
+public class Rating {
+    private float sum;
+    private int counter;
+    
+    public Rating() {
+    	this.sum = 0;
+    	this.counter = 0;
+    }
+    public void update(float rating){
+    	this.sum += rating;
+        this.counter ++;
+    }
+    public double value() {
+    	if (counter==0){
+    		return -1;
+    	}
+    	else {
+    		return (double)sum/counter;
+    	}
+    }
+}
