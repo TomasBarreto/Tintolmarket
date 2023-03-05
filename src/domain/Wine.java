5b5268686a742ad54ffef361c15ec3da3201ecd6
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-03-05 08:08:39 +0000

    fix: fixing indentation and naming
    url -> imageUrl // medRating -> averageRating

--- src/domain/Wine.java
+++ src/domain/Wine.java
@@ -1,22 +1,27 @@
 package src.domain;
 
 public class Wine {
+	
 	private final String name;
-    private final String url;
-    private Rating medRating;
+    private final String imageUrl;
+    private Rating averageRating;
     private int quantity;
+    
     public Wine(String name, String url){
         this.name = name;
-        this.url = url;
-        this.medRating = new Rating();
+        this.imageUrl = url;
+        this.averageRating = new Rating();
         this.quantity = 0;
     }
+    
     public void updateClassification(float rating){
-    	this.medRating.update(rating);
+    	this.averageRating.update(rating);
     }
+    
     public void add(int value) {
     	this.quantity += value;
     }
+    
     public void remove(int value) {
     	this.quantity -= value;
     }
@@ -24,10 +29,12 @@ public class Wine {
     public int quantity() {
     	return this.quantity;
     }
+    
     public String view() {
     	System.out.println("Wine " + this.name + ":");
-    	System.out.println("	Associated Image: "+ this.url);
-    	System.out.println("	Average Rating: "+ this.medRating.value());
+    	System.out.println("	Associated Image: "+ this.imageUrl);
+    	System.out.println("	Average Rating: "+ this.averageRating.value());
+    	
     	if(quantity==0) {
     		System.out.println("	There are no units available for sale.");
     	}
