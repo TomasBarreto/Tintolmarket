eeec8f3bc6f7a7a68e529d2f427134ca2d01538f
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-03-05 16:21:25 +0000

    add: adding parameter to interfaces functions
    adding ObjectOutputStream as a parameter to all functions in Tintolmarket and Tintolmarket Server Interfaces

--- src/interfaces/ITintolmarketServerStub.java
+++ src/interfaces/ITintolmarketServerStub.java
@@ -1,22 +1,25 @@
 package src.interfaces;
 
 import src.domain.Wine;
+
+import java.io.ObjectOutputStream;
+
 import src.domain.User;
 
 public interface ITintolmarketServerStub {
-	public void addWine(Wine wine, Image image);
+	public void addWine(Wine wine, Image image, ObjectOutputStream outStream);
 	
-	public void sellWine(Wine wine, int value, int quantity);
+	public void sellWine(Wine wine, int value, int quantity, ObjectOutputStream outStream);
 	
-	public void viewWine(Wine wine);
+	public void viewWine(Wine wine, ObjectOutputStream outStream);
 	
-	public void buyWine(Wine wine, User seller, int quantity);
+	public void buyWine(Wine wine, User seller, int quantity, ObjectOutputStream outStream);
 	
-	public void viewWallet();
+	public void viewWallet(ObjectOutputStream outStream);
 	
-	public void classifyWine(Wine wine, int stars);
+	public void classifyWine(Wine wine, int stars, ObjectOutputStream outStream);
 
-	public void sendMessage(User user, String message);
+	public void sendMessage(User user, String message, ObjectOutputStream outStream);
 	
-	public void readMessages();
+	public void readMessages(ObjectOutputStream outStream);
 }
