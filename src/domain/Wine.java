97d3758e8b05d826bfc41552a475a8f1f252f2f2
Bruno Costa <82226653+BrunoHamilton44@users.noreply.github.com>
2023-03-06 16:22:02 +0000

    update wine

--- src/domain/Wine.java
+++ src/domain/Wine.java
@@ -7,41 +7,26 @@ public class Wine {
 	private final String name;
     private final String imageUrl;
     private Rating averageRating;
-    private int quantity;
+
     
     public Wine(String name, String url){
         this.name = name;
         this.imageUrl = url;
         this.averageRating = new Rating();
-        this.quantity = 0;
+
     }
     
     public void updateClassification(float rating){
     	this.averageRating.update(rating);
     }
     
-    public void add(int value) {
-    	this.quantity += value;
-    }
+ 
     
-    public void remove(int value) {
-    	this.quantity -= value;
-    }
-
-    public int quantity() {
-    	return this.quantity;
-    }
-    
-    public String view() {
-    	System.out.println("Wine " + this.name + ":");
-    	System.out.println("	Associated Image: "+ this.imageUrl);
-    	System.out.println("	Average Rating: "+ this.averageRating.value());
-    	
-    	if(quantity==0) {
-    		System.out.println("	There are no units available for sale.");
-    	}
-    	else {
-    		//utilizador
-    	}
+    public String wineInfo() {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("Wine " + this.name + ":\n");
+    	sb.append("	Associated Image: "+ this.imageUrl+"\n");
+    	sb.append("	Average Rating: "+ this.averageRating.value()+"\n");
+    	return sb.toString();
     }
 }
