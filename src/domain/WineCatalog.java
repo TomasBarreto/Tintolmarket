06bbfa168e6d1bc0e7c57df3459bb3b7ddd85ea8
diogo02pereira <diogo02pereira@gmail.com>
2023-03-06 22:00:00 +0000

    Revert "Update WineCatalog.java"
    This reverts commit 183ed89e5a2a7d2692bcb8cc203cfcaf112f319d.

--- src/domain/WineCatalog.java
+++ src/domain/WineCatalog.java
@@ -4,32 +4,33 @@ package src.domain;
 import java.util.HashMap;
 
 public class WineCatalog {
-	private HashMap<String, Wine> wineCat;
-
+	private HashMap<String,Wine> wineCat;
+	
 	public WineCatalog() {
-		this.wineCat = new HashMap<String, Wine>();
+		this.wineCat = new HashMap<String,Wine>();	
 	}
-
+	
 	public void addWine(String name, String imageUrl) {
-		Wine newWine = new Wine(name, imageUrl);
+		Wine newWine = new Wine(name,imageUrl);
 	}
-
+	
 	public String getInfo(String name) {
 		return wineCat.get(name).wineInfo();
 	}
-
+	
 	public boolean containsWine(String name) {
 		return wineCat.containsKey(name);
 	}
-
+	
 	public boolean add(String nameWine, String imageUrl) {
-		if (!wineCat.containsKey(nameWine)) {
-			Wine newWine = new Wine(nameWine, imageUrl);
+		if(!wineCat.containsKey(nameWine)) {
+			Wine newWine = new Wine (nameWine,imageUrl);
 			wineCat.put(nameWine, newWine);
 			return true;
-		} else {
+		}
+		else {
 			return false;
 		}
-
+		
 	}
-}
+
