40dd648ae0e7eee0d306ebcbc9eedbceec02b86a
Bruno Costa <82226653+BrunoHamilton44@users.noreply.github.com>
2023-03-06 16:23:12 +0000

    add: WineSeller

---
+++ src/domain/WineSeller.java
@@ -0,0 +1,13 @@
+package src.domain;
+
+public class WineSeller {
+	private String seller;
+	private int price;
+	private int quantity;
+	public WineSeller(String seller, int price, int quantity) {
+		this.name = seller;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+}
