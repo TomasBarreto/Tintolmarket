3e03a15e97cc8fad4e19e53c307083c6d5f0bd53
Bruno Costa <82226653+BrunoHamilton44@users.noreply.github.com>
2023-03-06 17:13:37 +0000

    update Wine

--- src/domain/Wine.java
+++ src/domain/Wine.java
@@ -1,32 +1,28 @@
 package src.domain;
 
-import src.domain.Rating;
-
 public class Wine {
 	
 	private final String name;
-    private final String imageUrl;
-    private Rating averageRating;
+	private final String imageUrl;
+	private Rating averageRating;
 
-    
-    public Wine(String name, String url){
-        this.name = name;
-        this.imageUrl = url;
-        this.averageRating = new Rating();
+	
+	public Wine(String name, String url){
+		this.name = name;
+		this.imageUrl = url;
+		this.averageRating = new Rating();
 
-    }
-    
-    public void updateClassification(float rating){
-    	this.averageRating.update(rating);
-    }
-    
- 
-    
-    public String wineInfo() {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("Wine " + this.name + ":\n");
-    	sb.append("	Associated Image: "+ this.imageUrl+"\n");
-    	sb.append("	Average Rating: "+ this.averageRating.value()+"\n");
-    	return sb.toString();
-    }
+	}
+	
+	public void updateClassification(float rating){
+		this.averageRating.update(rating);
+	}
+	
+	public String wineInfo() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Wine " + this.name + ":\n");
+		sb.append("	Associated Image: "+ this.imageUrl+"\n");
+		sb.append("	Average Rating: "+ this.averageRating.value()+"\n");
+		return sb.toString();
+	}
 }
