1fe141169c212b4fcd4cee44f267ae1b05c7d788
Bruno Costa <82226653+BrunoHamilton44@users.noreply.github.com>
2023-03-06 16:24:00 +0000

    add: SellerList

---
+++ src/domain/SellerList.java
@@ -0,0 +1,13 @@
+package src.domain;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class SellerList {
+	private HashMap<String,List<WineSeller>> listSeller;
+	
+	public SellerList() {
+		this.listSeller = new HashMap <String,List<WineSeller>>();
+	}
+	
+}
