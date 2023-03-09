2f646f338113d12e8ea090c7792624a255cf6e9c
diogo02pereira <diogo02pereira@gmail.com>
2023-03-09 14:51:07 +0000

    Update TintolmarketStub.java
    remove unnecessary variable added to cmd

--- src/domain/TintolmarketStub.java
+++ src/domain/TintolmarketStub.java
@@ -57,7 +57,6 @@ public class TintolmarketStub implements ITintolmarketStub {
 		cmd.setWine(wine);
 		cmd.setWinePrice(value);
 		cmd.setWineQuantity(quantity);
-		cmd.setUser(userID);
 		
 		try {
 			outStream.writeObject(cmd);
@@ -150,7 +149,6 @@ public class TintolmarketStub implements ITintolmarketStub {
 	public String readMessages() throws IOException, ClassNotFoundException {
 		Command cmd = new Command();
 		cmd.setCommand("read");
-		cmd.setUser(userID);
 		
 		try {
 			outStream.writeObject(cmd);
