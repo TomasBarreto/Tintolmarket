1f1bb8b7930f883ea30f45026d82243679043681
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-03-12 15:58:57 +0000

    update Writing to wine files

--- src/domain/WineFileHandler.java
+++ src/domain/WineFileHandler.java
@@ -1,38 +1,58 @@
 package src.domain;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class WineFileHandler {
-	private static final String WINE_FILE = "wines.txt";
-	private static final String PERMISSIONS = "rw";
-	private RandomAccessFile handler;
 	
+	private static final String WINE_CAT_FILE = "wine_cat.txt";
+	private static final String WINE_SELLERS_FILE = "wine_sellers.txt";
+	private File wineCat;
+	private File wineSellers;
+
 	public WineFileHandler() {
-		try {
-			this.handler = new RandomAccessFile(new File(WINE_FILE), PERMISSIONS);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		this.wineCat = new File(WINE_CAT_FILE);
+		this.wineSellers = new File(WINE_SELLERS_FILE);
 	}
 	
-	public synchronized void alterFile(Command operation) {
-		switch(operation.getCommand()) {
-			case "addWine":			
-									break;
-			case "updateSeller":
-									break;
-			case "addSeller":
-									break;
-			case "updateRating":
-									break;
-			case "deleteSeller":
-									break;
-			case "updateQuantity":
-									break;
-									
-			default: 				break;
-		}
-	}
+	public synchronized void alterFile(Command cmd) {
+        try{
+            Scanner sc1 = new Scanner(this.wineCat);
+            FileWriter fw1 = new FileWriter(WINE_CAT_FILE, true);
+            BufferedWriter bw1 = new BufferedWriter(fw1);
+            
+            Scanner sc2 = new Scanner(this.wineSellers);
+            FileWriter fw2 = new FileWriter(WINE_SELLERS_FILE, true);
+            BufferedWriter bw2 = new BufferedWriter(fw2);
+
+            switch(cmd.getCommand()) {
+                case "addWine":			bw1.write(cmd.getWine() + ":" + 
+                						cmd.getImage() + ":" + "0" + ":" + "0" + "\n");
+                
+					                    break;
+                case "updateSeller":	
+                    break;
+                case "addSeller":		bw2.write(cmd.getWine() + ":" + cmd.getWineSeller() + 
+                						":" + cmd.getWineQuantity() + ":" + cmd.getWinePrice() + "\n");
+                
+                    					break;
+                case "updateRating":
+                    					break;
+                case "deleteSeller":
+                    					break;
+                case "updateQuantity":
+                    					break;
+
+                default:                break;
+            }
+
+        } catch (IOException e){
+            System.out.println("File not found");
+        }
+
+    }
 }
